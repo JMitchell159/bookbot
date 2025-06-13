ee35e246d42da34e206c6a93e8fa869ad8d65f06
JMichell159 <joemitchell159@gmail.com>
2025-06-13 18:41:56 -0500

    added user input functionality to main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import num_words
 from stats import num_char_sorted
 
@@ -8,7 +9,22 @@ def get_book_text(file):
     return contents
 
 def main():
-    file = "books/frankenstein.txt"
+    books = "book_directory.txt"
+    with open(books) as b:
+        book_list = b.read().split()
+    for i in range(len(book_list)):
+        book_list[i] = f"books/{book_list[i]}"
+    
+    if len(sys.argv) == 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    if len(sys.argv) > 2:
+        print("Usage: python3 main.py <path_to_book>. Warning: All other arguments will be ignored")
+    file = sys.argv[1]
+    if file not in book_list:
+        print("You must enter a valid book path")
+        sys.exit(1)
+    
     sorted_chars = num_char_sorted(get_book_text(file))
 
     print("============ BOOKBOT ============")
