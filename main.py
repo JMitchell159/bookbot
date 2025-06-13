5c0fe87e5a3a1e27ced5ef66a0f6f5101891f655
JMichell159 <joemitchell159@gmail.com>
2025-06-13 07:50:25 -0500

    add stats, moved num_words to stats

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+from stats import num_words
+
+def get_book_text(file):
+    with open(file) as f:
+        contents = f.read()
+    
+    return contents
+
+def main():
+    print(f"{num_words(get_book_text("books/frankenstein.txt"))} words found in the document")
+
+main()
